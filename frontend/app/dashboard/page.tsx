0ce91c301f561b56bb1587cb860379f8e7bdd870
Anthony Flores <anthonyj.floresm@gmail.com>
2026-06-13 13:25:46 +0200

    feat: update projects&tasks table dashboard

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -6,8 +6,8 @@ import { useState } from "react"
 export default function Dashboard() {
     const router = useRouter()
     const [user, setUser] = useState<{ name: string, email: string } | null>(null)
-    const [projects, setProjects] = useState<{ id: number, name: string }[]>([])
-    const [tasks, setTasks] = useState<{ id: number, name: string }[]>([])
+    const [projects, setProjects] = useState<{ id: number, name: string, status: string }[]>([])
+    const [tasks, setTasks] = useState<{ id: number, name: string, status: string}[]>([])
 
 
     useEffect(() => {
@@ -35,9 +35,9 @@ export default function Dashboard() {
         }
 
         const fetchTasks = async () => {
-            const response = await fetch ('http://localhost:4821/tasks',{
+            const response = await fetch('http://localhost:4821/tasks', {
                 method: 'GET',
-                headers: {'Authorization': `Bearer ${token}`}
+                headers: { 'Authorization': `Bearer ${token}` }
             })
 
             const data = await response.json()
@@ -50,10 +50,51 @@ export default function Dashboard() {
     }, [])
 
     return (
-        <div>
+        <div className="p-5">
             {user && <h1>Hello {user.name}</h1>}
-            {projects && projects.map((project) => <div key={project.id}>{project.name}</div>)}
-            {tasks && tasks.map((task) => <div key={task.id}>{task.name}</div> )}
+            <div className="flex flex-col md:flex-row gap-8 m-8">
+                {/* Projects Section*/}
+                <div className="flex-1">
+                    <h2>Recent Projects</h2>
+                    <table className="w-full border-collapse border">
+                        <thead className="bg-amber-100">
+                            <tr>
+                                <th className="text-left py-3 px-4">Name</th>
+                                <th className="text-left py-3 px-4">State</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {projects.map((project) => (
+                                <tr key={project.id} className="hover:bg-gray-50">
+                                    <td className="py-3 px-4 text-sm">{project.name}</td>
+                                    <td className="py-3 px-4 text-sm">{project.status}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+
+                {/* Tasks Section */}
+                <div className="flex-1">
+                    <h2>Current Tasks</h2>
+                    <table className="w-full border-collapse border">
+                        <thead className="bg-amber-100">
+                            <tr>
+                                <th className="text-left py-3 px-4">Name</th>
+                                <th className="text-left py-3 px-4">State</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {tasks.map((task) => (
+                                <tr key={task.id} className="hover:bg-gray-50">
+                                    <td className="py-3 px-4 text-sm">{task.name}</td>
+                                    <td className="py-3 px-4 text-sm">{task.status}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     )
 }
