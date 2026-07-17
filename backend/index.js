6c019a055a111df0fa2fd97391585a9cc9ad17ee
Anthony Flores <anthonyj.floresm@gmail.com>
2026-07-17 10:06:05 +0200

    debug: log request body

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,11 @@ dotenv.config()
 const port = process.env.PORT || 4821
 const app = express()
 
+app.use((req, res, next) => {
+    console.log('BODY:', req.body)
+    next()
+})
+
 
 app.use(cors({ origin: ['http://localhost:3000', 'https://essedi-production-6264.up.railway.app'] }))
 
